380a1b4a76088a938550261e204a9befa0e9eacf
kgs90 <kgs90@inbox.ru>
2022-04-19 22:05:17 +0300

    zadacha4

---
+++ Zadacha 4/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("введите число дня недели от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <  6 && number >= 1)
+{
+    System.Console.WriteLine("рабочий день");
+}
+else if(number == 6 || number == 7) 
+{
+System.Console.WriteLine("выходной");
+}
+else 
+{
+    System.Console.WriteLine("неверный запрос.");
+}
