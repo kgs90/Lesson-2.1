da385d089f040cf684d0d8336c0295caf13a0488
kgs90 <kgs90@inbox.ru>
2022-04-19 22:02:13 +0300

    zadacha3

---
+++ Zadacha 3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("введите число :");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n>99)
+{
+System.Console.WriteLine(n.ToString()[2]);
+}
+else
+{
+   System.Console.WriteLine("у числа не хватает цифры");
+}
