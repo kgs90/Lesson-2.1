523316a24c96e2b982e96fd021632eec80c59388
kgs90 <kgs90@inbox.ru>
2022-04-19 21:50:51 +0300

    ppc

---
+++ Zadacha 1/Program.cs
@@ -0,0 +1,4 @@
+﻿System.Console.WriteLine("Введите трехзначное число -");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number /10 % 10);
+Console.Write($"Вторая цифра - {(number /10 % 10)}");
